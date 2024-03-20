0c3f2f420fd11f5171483266da02a5f4719ab261
Takku <farelvivo06@gmail.com>
2024-03-21 02:06:20 +0700

    contoh library sederhana composer

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Takku\Belajar;
+
+class Customer
+{
+
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name = "Guest"): string
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+}
